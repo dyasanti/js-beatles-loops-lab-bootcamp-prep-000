ecb97e1301bbba9412a9a76d1db521ca83dcbeff
Dea  V <506thinfantry@gmail.com>
2018-05-03 20:42:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array = []; // MUST create new array 
+  let i = 0 
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++; // required if looping arrays
+  }
+  return array
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  let i = 0
+  do {
+    console.log("I love the Beatles!");
+    i++;
+  } while (n < 15);
+  return array
+}
